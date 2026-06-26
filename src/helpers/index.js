6938aa5eaff0213443c24aba9ba8f6851a8e2298
Devansh Ganatra <devanshganatra9@gmail.com>
2026-06-26 17:32:45 +0530

    Initial backend setup

--- src/helpers/index.js
+++
@@ -1,6 +0,0 @@
-// Reusable business utilities will be placed here
-// e.g., fee calculations, academic year parsers
-
-export const calculateRemainingFee = (totalAmount, paidAmount, concessionAmount = 0) => {
-  return totalAmount - paidAmount - concessionAmount;
-};
